1244231a07cec257545522436304d163392c61c3
Jealyang <jaelyangchoi@gmail.com>
2021-01-22 22:04:00 +0900

    로그인 구현

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 5000;
 const { User } = require("./models/User"); //객체를 반환하는 것이므로. {} 속 알맹이를 User로 가리키는 효과. User=Model{User}
 const bodyParser = require("body-parser"); //body data를 parsing해서 req.body로 출력해줌
+const cookieParser = require("cookie-parser");
 const mongoose = require("./lib/db.js");
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,14 +15,46 @@ app.get("/", (req, res) => {
 
 //endpoint register
 app.post("/register", (req, res) => {
-  //회원 가입할 때 필요한 정보들을 client에게 가져오면
-  //그것들을 db에 넣어준다.
+  //회원 가입할 때 필요한 정보들을 client에게 가져오면 db에 넣어준다.
+
+  //그때 그때 user 모델 생성
   const user = new User(req.body); //json으로 받는 경우
 
-  //User 모델에 저장 (mongodb의 메소드)
+  //model을 사용하여 데이터를 데이터베이스에 저장
   user.save((err, userInfo) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({ success: true, userInfo });
+  });
+});
+
+app.post("/login", (req, res) => {
+  //요청된 이메일이 DB에 있는지 찾는다.
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user)
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다.",
+      });
+
+    //있다면 비밀번호가 맞는지 확인
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 틀렸습니다.",
+        });
+
+      //비밀번호까지 맞다면 토큰 생성
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+
+        //쿠키에 토큰을 저장한다.
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSucces: true, userId: user._id });
+      });
+    });
   });
 });
 
--- models/User.js
+++ models/User.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
-
+const jwt = require("jsonwebtoken");
+/*
+schema: 해당 컬렉션의 document에 어떤 종류의 값이 들어가는지 정의
+schema는 document의 구조가 어떻게 생겼는지 알려주는 역할.
+*/
 const userSchema = mongoose.Schema({
   name: {
     type: String,
@@ -39,7 +43,7 @@ const userSchema = mongoose.Schema({
 arrow function을 쓰면 상위 스코프의 this를 가리킴 -> 여기선 global
 */
 
-//저장 전에 비밀번호 암호화
+//저장 전에 비밀번호 암호화. 미들웨어이고 save()전에 호출되니까 next() 있어야 됨
 userSchema.pre("save", function (next) {
   //메소드의 경우 this는 객체를 가리킨다.
   //자바스크립트의 함수는 호출될 때, 매개변수로 전달되는 인자값 이외에, arguments 객체와 this를 암묵적으로 전달 받는다.
@@ -53,9 +57,37 @@ userSchema.pre("save", function (next) {
         next();
       });
     });
-  }
+  } else next(); //next 없으면 여기서 머물게 된다.
 });
-//모델: 스키마를 통해 만드는 인스턴스
+
+userSchema.methods.comparePassword = function (plainPassword, callback) {
+  //plainPassword를 암호화해서 비교
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return callback(err);
+    callback(null, isMatch);
+  });
+};
+
+//user._id를 이용해 token 생성. 보안을 위해 user._id를 노출시키지 않으려고.
+userSchema.methods.generateToken = function (callback) {
+  //jsonwebtoken을 이용해서 token을 생성하기
+  let user = this;
+  let token = jwt.sign(user._id.toHexString(), "secretToken");
+  //user._id 와 'secretToken'을 합하여 token을 만든다.
+  //-> 이후 'secretToken'을 이용해 토큰으로부터 user._id 찾는다!!
+
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return callback(err);
+    callback(null, user);
+  });
+};
+/*
+model: 스키마를 통해 만드는 인스턴스
+model은 데이터베이스에서 데이터를 읽고, 생성하고, 수정하는 프로그래밍 인터페이스를 정의
+model을 사용하여 데이터를 데이터베이스에 저장하거나 조회 할 수 있다.
+*/
 const User = mongoose.model("User", userSchema); //모델의 이름과 스키마
+//각 User 객체는 각자의 schema를 갖으며, userSchema를 이용해 객체화했으므로, userSchema의 메소드를 쓸 수 있다.
 
 module.exports = { User }; //프로퍼티: User, 값: Model { User } 객체
