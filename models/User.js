4430385d1890bf76e31856db675dd5991732657f
Jealyang <jaelyangchoi@gmail.com>
2021-01-22 12:39:36 +0900

    User 모델 생성

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: 1,
+  },
+  password: {
+    type: String,
+    minlength: 5,
+  },
+  lastname: {
+    type: String,
+    maxlength: 50,
+  },
+  role: {
+    type: Number,
+    default: 0,
+  },
+  image: String,
+  token: {
+    type: String,
+  },
+  tokenExp: {
+    type: Number,
+  },
+});
+
+//모델: 스키마를 통해 만드는 인스턴스
+const User = mongoose.model("User", userSchema); //모델의 이름과 스키마
+
+module.exports = { User };
