b51efd0246c79641e878727349d2802a72180ffb
Jealyang <jaelyangchoi@gmail.com>
2021-01-22 12:36:12 +0900

    mongoose 적용, mongodb 연결

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const db_config = require("./config/mongoose.json");
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    `mongodb+srv://jaelyang:${db_config.password}@boilerplate.rgdkh.mongodb.net/<dbname>?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("successfully connected to mongodb"))
+  .catch((err) => console.log(err));
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
