e36361bc7bc5a55735db96e694c6e8d66b3a6da3
Jealyang <jaelyangchoi@gmail.com>
2021-01-25 15:04:02 +0900

    logout button 구현

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -2,11 +2,17 @@ import React, { useEffect } from "react";
 import axios from "axios";
 
 // You can think of these components as "pages"
-function LandingPage() {
+function LandingPage(props) {
   useEffect(() => {
     axios.get("/api/hello").then((response) => console.log(response));
   }, []);
 
+  const onLogoutHandler = () => {
+    axios.get("/api/users/logout").then((response) => {
+      if (response.data.success) props.history.push("/login");
+      else alert("Failed to logout");
+    });
+  };
   return (
     <div
       style={{
@@ -18,6 +24,7 @@ function LandingPage() {
       }}
     >
       <h2>시작 페이지</h2>
+      <button onClick={onLogoutHandler}>logout</button>
     </div>
   );
 }
